754e6381b111b5925327763171c197424299819e
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-02 16:37:55 -0400

    sql scripts

---
+++ sql scripts/Base Ingredient Script.sql
@@ -0,0 +1,421 @@
+-- Hops ----------------------------------------------------------------------------------
+-- Citra
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Citra', 
+    'U.S.', 
+    'O', 
+    12.4,
+    4,
+    3.25,
+    'Strong citrus and tropical tones of grapefruit, melon, lime, gooseberry, passion fruit and lychee.'
+);
+-- Amarillo
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Amarillo', 
+    'U.S.', 
+    'A', 
+    10,
+    7,
+    3.30,
+    'Floral, tropical, and citrus (lemon, orange and grapefruit) characteristics.'
+);
+-- Galaxy
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Galaxy', 
+    'Australia', 
+    'A', 
+    14,
+    6,
+    3.50,
+    'Typically used as a late addition - the aroma and flavor characteristics become more pronounced the later the addition. Distinct citrus, peach and passion fruit flavors.'
+);
+-- Azacca
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Azacca', 
+    'U.S.', 
+    'O', 
+    12,
+    5,
+    3.00,
+    'Named for the Haitian god of agriculture, Azacca, it boasts an intense and tropical aroma. Sustained impressions of citrus and very ripe mango, with notes of orchard fruit (pears, apples) and pine needles throughout. These excellent aromatic qualities make Azacca™ a go-to hop for late and dry hop additions in a variety of styles, although its high myrcene fraction has already made it a favorite of IPA brewers. Shows well as a single hop in a simple grist.'
+);
+-- Cascade
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Cascade', 
+    'U.S.', 
+    'O', 
+    6,
+    6,
+    2.25,
+    'Flowery, citrus & spice with a noticeable grapefruit fragrance. Cascade has a medium aroma which balances the low bittering value. Derived from a cross between fuggles and the Russian hop Serebrianker. Very popular hop among craft brewers.'
+);
+-- Centennial
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Centennial', 
+    'U.S.', 
+    'O', 
+    10,
+    4,
+    2.45,
+    'Flowers & citrus are the most evident flavours. A medium aroma with mid to high bittering value makes it a dual purpose choice. Cross between Brewer''s Gold, Fuggle and East Kent Golding.'
+);
+-- Chinook
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Chinook', 
+    'U.S.', 
+    'B', 
+    13,
+    3.5,
+    2.30,
+    'Distinct, medium intensity spice and pine characteristics with subtle notes of grapefruit.'
+);
+-- Comet
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Comet', 
+    'U.S.', 
+    'A', 
+    10.5,
+    4.5,
+    2.85,
+    'Comet is a cross of English hops with a wild American variety and was first released in 1974. Commercial production actually ceased in the 1980s as it was phased out in favor of new super-high alpha varieties, but it has enjoyed a comeback thanks to its unique profile of citrus zest and fragrant grassy character, with undertones of wild forest fruit and old-school earthy bitterness.'
+);
+-- Saaz
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Saaz', 
+    'Czech Republic', 
+    'O', 
+    3.5,
+    7.5,
+    2.75,
+    'Very mild, with pleasant earthy, herbal and floral overtones.'
+);
+-- East Kent Goldings
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'East Kent Goldings', 
+    'U.K.', 
+    'O', 
+    5,
+    2.5,
+    2.55,
+    'A traditional English hop that has been used in kettle and dry hopping. Smooth and delicate with floral, lavender, spice, honey, earth, lemon and thyme overtones.'
+);
+-- El Dorado
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'El Dorado', 
+    'U.S.', 
+    'O', 
+    15,
+    7.5,
+    2.95,
+    'El Dorado is an exceptional new dual purpose hop with both high Alpha Acid and intense flavors and aromas. As a bittering hop El Dorado lends a firm but balanced bitterness. This hop has bright tropical fruit flavors and aromas of pear, watermelon, and stone fruit.'
+);
+-- Fuggle
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Fuggle', 
+    'U.K.', 
+    'A', 
+    5,
+    2,
+    2.45,
+    'A traditional English variety of hop with hints of mild wood and fruity characteristics.'
+);
+-- Hallertau Blanc
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Hallertau Blanc', 
+    'Germany', 
+    'A', 
+    10.5,
+    5,
+    3.95,
+    'One of several new German varieties with bold flavor profiles, perfect for American-style Ales. Floral and fruity with passion fruit, grapefruit, pineapple, grape and lemongrass overtones. The daughter of Cascade.'
+);
+-- Hallertau Mittelfruh
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Hallertau Mittelfruh', 
+    'Germany', 
+    'A', 
+    4,
+    4,
+    2.45,
+    'The classic German aroma hop associated with Bavarian-style lager beers and distinguished by an intense, pleasantly harmonic bitterness.'
+);
+-- Huell Melon
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Huell Melon', 
+    'Germany', 
+    'A', 
+    7.5,
+    7.5,
+    3.35,
+    'Distinct fruit characteristics including honeydew melon and strawberry flavors. Hull Melon is the daughter of Cascade.'
+);
+-- Magnum
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Magnum', 
+    'Germany', 
+    'B', 
+    14,
+    6,
+    2.50,
+    'A delicate aroma with inclinations towards floral and fruit flavorings and a prevalent hoppy tang.'
+);
+-- Northern Brewer
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Northern Brewer', 
+    'Germany', 
+    'O', 
+    8.5,
+    4.5,
+    2.45,
+    'A true dual purpose hop which is a cross between an American wild hop and an English male.'
+);
+-- Perle
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Perle', 
+    'Germany', 
+    'A', 
+    8,
+    3.5,
+    2.50,
+    'A highly acceptable aroma variety with balanced bittering characteristics. Delicate floral, fruit, spice and mint tones.'
+);
+-- Polaris
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Polaris', 
+    'Germany', 
+    'O', 
+    20,
+    5,
+    3.45,
+    'Bred at the Hop Research Institute in Hüll and released in 2012, Polaris was commercialized as a new, German variety in response to growing demand from the craft beer industry for distinct flavor profiles. It features extremely high alpha content along with intense mint, pineapple and menthol flavors.'
+);
+-- Spalt
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Spalt', 
+    'Germany', 
+    'A', 
+    2.6,
+    5.2,
+    3.25,
+    'Traditional noble aroma hop with average bitter value and pleasant aroma. This hop is only grown in the Spalt area of Germany. Traditionally used in Bocks. The aroma is mild and herbal, with spicy, floral and fruit tones.'
+);
+-- Tettnang
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Tettnang', 
+    'Germany', 
+    'A', 
+    4.5,
+    4,
+    3.00,
+    'Traditional noble aroma hop belonging to the Saaz family; mild and pleasant with balanced earthy, herbal and floral aroma.'
+);
+-- Mandarina Bavaria
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Mandarina Bavaria', 
+    'Germany', 
+    'O', 
+    8.5,
+    6,
+    3.25,
+    'Bred at the Hop Research Institute in Hüll, Mandarina Bavaria displays pleasant fruitiness in finished beer. Surprise, surprise it is known for having a fruity, citrusy, Mandarin Orange flavor. It is a daughter hop of Cascade grown in Germany and is also said to slightly enhance sweetness.'
+);
+-- Mosaic
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Mosaic', 
+    'U.S.', 
+    'A', 
+    12,
+    3.5,
+    3.25,
+    'A complex array of tropical fruit, citrus, berry, herbal, earthy and pine characteristics. The daughter of Simcoe hops.'
+);
+-- Motueka
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Motueka', 
+    'New Zealand', 
+    'A', 
+    7,
+    5.5,
+    3.25,
+    'An excellent variety for single hop bills with multiple applications. Lively lemon and lime tones with background hints of tropical fruit'
+);
+-- Vic Secret
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Vic Secret', 
+    'Australia', 
+    'A', 
+    15.5,
+    7,
+    3.25,
+    'Vic Secret has a clean and bright tropical character (passionfruit, pineapple) with supporting notes of fresh herbs and pine needles. Similar to Galaxy but lighter and less dominating. Developed in Australia from a seedling bred in Victoria in 2000 and known initially as Cultivar #00-207-013, Vic Secret shares parentage with Topaz. It was released for commercial production in 2013.'
+);
+-- Wakatu
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Wakatu', 
+    'New Zealand', 
+    'O', 
+    7.5,
+    8.5,
+    2.95,
+    'Popular for its rounded flavor, moderate co-humulone, outstanding oil profile and rewarding finish which has restrained floral notes and freshly zested lime.'
+);
+-- Warrior
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Warrior', 
+    'U.S.', 
+    'B', 
+    16,
+    5,
+    2.79,
+    'Mild and resinous with subtle citrus, pine and herbal characteristics.'
+);
+-- Willamette
+INSERT INTO hops(hop_name, hop_origin, hop_type, alpha, beta, hop_price, hop_notes) VALUES
+(
+	'Willammette', 
+    'U.S.', 
+    'A', 
+    5,
+    4,
+    2.95,
+    'Willamette is a triploid aroma type hop that for years was the most widely grown US aroma variety. Known for its citrus, floral, spicy and herbal notes.'
+);
+
+-- Yeast ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- West Coast Ale I
+INSERT INTO yeast(yeast_name, age_rate, product_id, lab, yeast_type, alcohol_tolerance, flocculation, min_attenuation, max_attenuation, min_temperature, max_temperature, yeast_price, yeast_notes) VALUES
+(
+	'West Coast Ale I',
+    21,
+    'OYL-004',
+    'Omega',
+    'Ale',
+    11,
+    'M',
+    73,
+    80,
+    16,
+    23,
+    9.50,
+    'Clean, crisp flavor characteristics with low fruitiness and mild ester production. A very versatile yeast for styles that desire dominant malt and hop character. A very popular “house” strain. May yield citrus notes with cooler 60-66°F fermentations.'
+);
+-- Bayern Lager
+INSERT INTO yeast(yeast_name, age_rate, product_id, lab, yeast_type, alcohol_tolerance, flocculation, min_attenuation, max_attenuation, min_temperature, max_temperature, yeast_price, yeast_notes) VALUES
+(
+	'Bayern Lager',
+    21,
+    'OYL-114',
+    'Omega',
+    'Lag',
+    10,
+    'M',
+    72,
+    76,
+    10,
+    17,
+    9.50,
+    'Lager strain hailing from Munich’s oldest brewery. It produces low sulfur and low diacetyl and works in a wide temperature range, resulting in a clean, crisp lager. The most widely used lager strain in the world.'
+);
+-- Tropical IPA
+INSERT INTO yeast(yeast_name, age_rate, product_id, lab, yeast_type, alcohol_tolerance, flocculation, min_attenuation, max_attenuation, min_temperature, max_temperature, yeast_price, yeast_notes) VALUES
+(
+	'Tropical IPA',
+    21,
+    'OYL-200',
+    'Omega',
+    'Dia',
+    10,
+    'L',
+    85,
+    90,
+    24,
+    29,
+    9.50,
+    'A unique Saccharomyces strain formerly classified as Brettanomyces that produces delicate, tart, tropical, mango and pineapple fruit characteristics.'
+);
+-- British Ale I
+INSERT INTO yeast(yeast_name, age_rate, product_id, lab, yeast_type, alcohol_tolerance, flocculation, min_attenuation, max_attenuation, min_temperature, max_temperature, yeast_price, yeast_notes) VALUES
+(
+	'British Ale I',
+    21,
+    'OYL-006',
+    'Omega',
+    'Ale',
+    10,
+    'H',
+    70,
+    80,
+    17,
+    22,
+    9.50,
+    'OLY-006 allows malt and hop character to dominate the profile. Highly flocculant and highly attenuative. Ferments well down to 64°F.'
+);
+
+-- Fermentables ---------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO fermentables(ferm_name, ferm_origin, ferm_type, potential_gravity, colour, diastatic_power, protein_content, ferm_price, ferm_notes) VALUES
+(
+	'2-Row',
+    'Canada',
+    'B',
+    1.036,
+    1.95,
+    140.0,
+    12.5,
+    1.28,
+    '2-row barley malt is probably the most commonly used grain by brewers. This malt is modified and high in enzymes that are essential for your yeast. Easily converts sugars with a single infusion mash. A great base malt with moderate malt flavour.'
+);
+
+-- Water --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO water(water_name, ph, alkalinity, effective_hardness, residual_alkalinity, calcium, magnesium, sodium, sulfate, chloride, bicarbonate, water_price, water_notes) VALUES
+(
+	'Kitchener',
+    7.92,
+    246,
+    78,
+    168,
+    88,
+    25,
+    32,
+    50,
+    83,
+    300,
+    0,
+    'Kitchener, Ontario water profile. Good for malt foreward styles. Required additives to bring out more hop character. Not recommended for styles with muted malt character.'
+);
+
+-- Misc --------------------------------------------------------------------------
+INSERT INTO misc(misc_name, measurement, misc_type, misc_price, misc_notes) VALUES
+(
+	'Whirlfloc Tablet',
+    'U',
+    'Fi',
+    0.30,
+    'A clarifying agent used in brewing during the boil. It is a concentrated tablet derived from Irish Moss and carrageenan that aids in the coagulation and settling of haze-producing proteins and beta glucans.. Add 1/2 tablet per 5 gallon batch within 5 minutes of boil time remaining to promote clarity.'
+);
---
+++ sql scripts/hopstack schema.sql
@@ -0,0 +1,262 @@
+-- Initilize Tables
+CREATE TABLE hops 
+(
+    hop_id INT AUTO_INCREMENT, 
+    hop_name VARCHAR(20) NOT NULL, 
+    hop_origin VARCHAR(20), -- Country of origin
+    hop_type VARCHAR(1), -- A = Aroma, B = Bittering, O = Both
+    alpha DECIMAL(4, 2), -- Alpha acid content in %
+    beta DECIMAL(4, 2), -- Beta acid content in %
+    hop_price DECIMAL (4, 2) DEFAULT 0, 
+    hop_qty DECIMAL(5, 2) DEFAULT 0,
+    hop_notes VARCHAR(1000),
+    PRIMARY KEY(hop_id)
+);
+
+CREATE TABLE yeast 
+(
+    yeast_id INT AUTO_INCREMENT,
+    yeast_name VARCHAR(20) NOT NULL, 
+    production_date DATE DEFAULT (CURRENT_DATE()), 
+    age_rate INT DEFAULT 21, -- % viability lost every month
+    product_id VARCHAR(10), -- Lab provided product id
+    lab VARCHAR(20), -- Lab of origin
+    yeast_type VARCHAR(3), -- Ale, Lag, Brt, Dia, Ped, Lac
+    alcohol_tolerance INT, -- Alcohol tolerance in %
+    flocculation VARCHAR(1), -- L = Low, M = Medium, H = High
+    min_attenuation INT, -- Minimum attenuation in &
+    max_attenuation INT, -- Maximum attenuation in %
+    min_temperature INT, -- Minimum temperature in C
+    max_temperature INT, -- Maximum temperature in C
+    yeast_price DECIMAL(4, 2) DEFAULT 0, 
+    yeast_qty DECIMAL(5, 2) DEFAULT 0,
+    yeast_notes VARCHAR(1000), 
+    PRIMARY KEY(yeast_id)
+);
+
+CREATE TABLE fermentables
+(
+	ferm_id INT AUTO_INCREMENT,
+	ferm_name VARCHAR(20) NOT NULL,
+	ferm_origin VARCHAR(20), -- Country of origin
+	ferm_type VARCHAR(1), -- B = Base, S = Specialty, L = Liquid Extract, D = Dry Extract, U = Sugar, Y = Syrup, J = Juice, F = Fruit, O = Other
+	potential_gravity DECIMAL(4, 3), -- Base gravity potential for ferm_type B, S / Specific gravity for ferm_type L, D, U, Y, J, F, O
+	colour DECIMAL(4, 2), -- Colour contribution in SRM
+	diastatic_power DECIMAL(4, 1), -- Measure of enzyme content in Litner for ferm_type B, S
+	protein_content DECIMAL(3, 1), -- Measure of protein content in % for ferm_type B, S
+	ferm_price DECIMAL(4, 2) DEFAULT 0,
+	ferm_qty DECIMAL(5, 2) DEFAULT 0,
+	ferm_notes VARCHAR(1000),
+	PRIMARY KEY(ferm_id)
+);
+
+CREATE TABLE water
+(
+	water_id INT AUTO_INCREMENT,
+	water_name VARCHAR(20) NOT NULL,
+	ph DECIMAL(3, 2),
+	alkalinity INT, -- ppm as CaCO3
+	effective_hardness INT, -- ppm as CaCO3
+	residual_alkalinity INT, -- ppm as CaCO3
+	calcium DECIMAL(5, 2), -- ppm
+	magnesium DECIMAL(5, 2), -- ppm
+	sodium DECIMAL(5, 2), -- ppm
+	sulfate DECIMAL(5, 2), -- ppm
+	chloride DECIMAL(5, 2), -- ppm
+	bicarbonate DECIMAL(5, 2), -- ppm
+	water_price DECIMAL(4, 2) DEFAULT 0,
+	water_qty DECIMAL(5, 2) DEFAULT 0,
+	water_notes VARCHAR(1000),
+	PRIMARY KEY(water_id)
+);
+
+CREATE TABLE misc
+(
+	misc_id INT AUTO_INCREMENT,
+	misc_name VARCHAR(20) NOT NULL,
+	measurement VARCHAR(1), -- V = Volume, W = Weight, U = Unit
+	misc_type VARCHAR(2), -- Fl = Flavour, Wa = Water Agent, Yn = Yeast Nutrient, En = Enzyme, Fi = Fining, Ot = Other
+	misc_price DECIMAL(4, 2) DEFAULT 0,
+	misc_qty DECIMAL(5, 2) DEFAULT 0,
+	misc_notes VARCHAR(1000),
+	PRIMARY KEY(misc_id)
+);
+
+CREATE TABLE inventory 
+(
+	inv_id INT AUTO_INCREMENT, 
+    name VARCHAR(20), 
+    type VARCHAR(1), 
+    qty DECIMAL(5, 2), 
+    price DECIMAL(4, 2),
+    hop_id INT DEFAULT NULL,
+    yeast_id INT DEFAULT NULL,
+    ferm_id INT DEFAULT NULL,
+    water_id INT DEFAULT NULL, 
+    misc_id INT DEFAULT NULL,
+    PRIMARY KEY(inv_id),
+    FOREIGN KEY(hop_id) REFERENCES hops(hop_id) ON DELETE CASCADE,
+    FOREIGN KEY(yeast_id) REFERENCES yeast(yeast_id) ON DELETE CASCADE,
+    FOREIGN KEY(ferm_id) REFERENCES fermentables(ferm_id) ON DELETE CASCADE,
+    FOREIGN KEY(water_id) REFERENCES water(water_id) ON DELETE CASCADE,
+    FOREIGN KEY(misc_id) REFERENCES misc(misc_id) ON DELETE CASCADE
+);
+
+CREATE TABLE recipe
+(
+	recipe_name VARCHAR(50),
+    iteration INT DEFAULT 1,
+	recipe_ingredients JSON,
+	recipe_total DECIMAL(6, 2),
+	functional_cost DECIMAL(6, 2),
+	recipe_notes VARCHAR(1000),
+    recipe_changes JSON,
+    PRIMARY KEY(recipe_name)
+);
+
+CREATE TABLE iteration_notes
+(
+	recipe_name VARCHAR(50),
+    iteration INT DEFAULT 1,
+    change_log JSON,
+    PRIMARY KEY(recipe_name, iteration),
+    FOREIGN KEY(recipe_name) REFERENCES recipe(recipe_name) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE list
+(
+	list_id INT AUTO_INCREMENT,
+	list_name VARCHAR(50),
+	timestamp DATETIME DEFAULT(NOW()),
+	list_ingredients JSON,
+	list_total DECIMAL(7, 2),
+	list_notes VARCHAR(1000),
+    PRIMARY KEY(list_id)
+);
+
+-- Set Triggers
+DELIMITER $$
+CREATE
+	TRIGGER add_hop AFTER INSERT
+    ON hops
+    FOR EACH ROW BEGIN
+		IF (NEW.hop_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, hop_id) VALUES(NEW.hop_name, 'h', NEW.hop_qty, NEW.hop_price, NEW.hop_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER update_hop AFTER UPDATE
+    ON hops
+    FOR EACH ROW BEGIN
+		IF (OLD.hop_qty = 0 AND NEW.hop_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, hop_id) VALUES(NEW.hop_name, 'h', NEW.hop_qty, NEW.hop_price, NEW.hop_id);
+		ELSEIF (OLD.hop_qty > 0 AND NEW.hop_qty = 0) THEN
+			DELETE FROM inventory WHERE(inventory.hop_id = NEW.hop_id);
+		ELSEIF (OLD.hop_qty > 0 AND NEW.hop_qty > 0) THEN
+			UPDATE inventory SET name = NEW.hop_name, qty = NEW.hop_qty, price = NEW.hop_price WHERE (inventory.hop_id = NEW.hop_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER add_yeast AFTER INSERT
+    ON yeast
+    FOR EACH ROW BEGIN
+		IF (NEW.yeast_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, yeast_id) VALUES(NEW.yeast_name, 'y', NEW.yeast_qty, NEW.yeast_price, NEW.yeast_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER update_yeast AFTER UPDATE
+    ON yeast
+    FOR EACH ROW BEGIN
+		IF (OLD.yeast_qty = 0 AND NEW.yeast_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, yeast_id) VALUES(NEW.yeast_name, 'y', NEW.yeast_qty, NEW.yeast_price, NEW.yeast_id);
+		ELSEIF (OLD.yeast_qty > 0 AND NEW.yeast_qty = 0) THEN
+			DELETE FROM inventory WHERE(inventory.yeast_id = NEW.yeast_id);
+		ELSEIF (OLD.yeast_qty > 0 AND NEW.yeast_qty > 0) THEN
+			UPDATE inventory SET name = NEW.yeast_name, qty = NEW.yeast_qty, price = NEW.yeast_price WHERE (inventory.yeast_id = NEW.yeast_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER add_ferm AFTER INSERT
+    ON fermentables
+    FOR EACH ROW BEGIN
+		IF (NEW.ferm_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, ferm_id) VALUES(NEW.ferm_name, 'f', NEW.ferm_qty, NEW.ferm_price, NEW.ferm_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER update_ferm AFTER UPDATE
+    ON fermentables
+    FOR EACH ROW BEGIN
+		IF (OLD.ferm_qty = 0 AND NEW.ferm_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, ferm_id) VALUES(NEW.ferm_name, 'f', NEW.ferm_qty, NEW.ferm_price, NEW.ferm_id);
+		ELSEIF (OLD.ferm_qty > 0 AND NEW.ferm_qty = 0) THEN
+			DELETE FROM inventory WHERE(inventory.ferm_id = NEW.ferm_id);
+		ELSEIF (OLD.ferm_qty > 0 AND NEW.ferm_qty > 0) THEN
+			UPDATE inventory SET name = NEW.ferm_name, qty = NEW.ferm_qty, price = NEW.ferm_price WHERE (inventory.ferm_id = NEW.ferm_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER add_water AFTER INSERT
+    ON water
+    FOR EACH ROW BEGIN
+		IF (NEW.water_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, water_id) VALUES(NEW.water_name, 'w', NEW.water_qty, NEW.water_price, NEW.water_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER update_water AFTER UPDATE
+    ON water
+    FOR EACH ROW BEGIN
+		IF (OLD.water_qty = 0 AND NEW.water_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, water_id) VALUES(NEW.water_name, 'w', NEW.water_qty, NEW.water_price, NEW.water_id);
+		ELSEIF (OLD.water_qty > 0 AND NEW.water_qty = 0) THEN
+			DELETE FROM inventory WHERE(inventory.water_id = NEW.water_id);
+		ELSEIF (OLD.water_qty > 0 AND NEW.water_qty > 0) THEN
+			UPDATE inventory SET name = NEW.water_name, qty = NEW.water_qty, price = NEW.water_price WHERE (inventory.water_id = NEW.water_id);
+		END IF;
+	END$$
+    
+CREATE
+	TRIGGER add_misc AFTER INSERT
+    ON misc
+    FOR EACH ROW BEGIN
+		IF (NEW.misc_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, misc_id) VALUES(NEW.misc_name, 'm', NEW.misc_qty, NEW.misc_price, NEW.misc_id);
+		END IF;
+	END$$
+
+CREATE
+	TRIGGER update_misc AFTER UPDATE
+    ON misc
+    FOR EACH ROW BEGIN
+		IF (OLD.misc_qty = 0 AND NEW.misc_qty > 0) THEN
+			INSERT INTO inventory(name, type, qty, price, misc_id) VALUES(NEW.misc_name, 'm', NEW.misc_qty, NEW.misc_price, NEW.misc_id);
+		ELSEIF (OLD.misc_qty > 0 AND NEW.misc_qty = 0) THEN
+			DELETE FROM inventory WHERE(inventory.misc_id = NEW.misc_id);
+		ELSEIF (OLD.misc_qty > 0 AND NEW.misc_qty > 0) THEN
+			UPDATE inventory SET name = NEW.misc_name, qty = NEW.misc_qty, price = NEW.misc_price WHERE (inventory.misc_id = NEW.misc_id);
+		END IF;
+	END$$
+    
+CREATE
+	TRIGGER add_recipe AFTER INSERT
+    ON recipe
+    FOR EACH ROW BEGIN
+		INSERT INTO iteration_notes(recipe_name, change_log) VALUES(NEW.recipe_name, '["Initial Recipe"]');
+	END$$
+    
+CREATE
+	TRIGGER update_recipe AFTER UPDATE
+    ON recipe 
+    FOR EACH ROW BEGIN
+		INSERT INTO iteration_notes(recipe_name, iteration, change_log) VALUES(NEW.recipe_name, NEW.iteration, NEW.recipe_changes);
+	END$$
+DELIMITER ;
