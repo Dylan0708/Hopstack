4fbf901552bc39bbef40dc86f5c1d97edb4e39b5
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-11 23:28:39 -0400

    added db connection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import Error
 
 #Log In Variables
 #username
@@ -6,3 +7,14 @@ import mysql.connector
 
 username = input("Enter Username: ")
 password = input("Enter Password: ")
+
+#connect to db, set cursor
+try:
+    db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = username, password = password)
+    if db_con.is_connected():
+        print('Connected to hopstack') 
+except Error as e:
+    print(e)
+finally:
+    if db_con is not None and db_con.is_connected():
+        db_con.close()
