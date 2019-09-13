9dfcc07ef0018195ff602fb3b76e0fa074577b07
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-13 00:00:39 -0400

    Added main menu after login

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import main_func
 import mysql.connector
 import getpass
 from mysql.connector import Error
-
-#Log In Variables
-#username
-#password
 
 username = input("Enter Username: ")
 password = getpass.getpass("Enter Password: ")
@@ -12,10 +9,13 @@ password = getpass.getpass("Enter Password: ")
 #connect to db, set cursor
 try:
     db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = username, password = password)
-    if db_con.is_connected():
-        print('Connected to hopstack') 
 except Error as e:
     print(e)
+
+#main menu
+main_func.cls()
+print ('Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients')
+menu_select = input("Select a Menu: ")
 
 if db_con is not None and db_con.is_connected():
     db_con.close()
