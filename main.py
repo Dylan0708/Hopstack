159af9ce457560eec1a8614c8fb09a09ae14e28a
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-14 00:22:19 -0400

    moved backend functionality into main_func

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import main_func
-import mysql.connector
-import getpass
-from mysql.connector import Error
 
 username = input("Enter Username: ")
-password = getpass.getpass("Enter Password: ")
+password = main_func.getpass.getpass("Enter Password: ")
 
-#connect to db, set cursor
-try:
-    db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = username, password = password)
-except Error as e:
-    print(e)
-
+#establish db connection
+con = main_func.hstack_connect(username, password)
 #main menu
-main_func.cls()
-print ('Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients')
-menu_select = input("Select a Menu: ")
+#main_func.cls()
+#print ('Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients')
+#menu_select = input("Select a Menu: ")
 
-if db_con is not None and db_con.is_connected():
-    db_con.close()
+if con is not None and con.is_connected():
+    con.close()
--- main_func.py
+++ main_func.py
@@ -1,4 +1,34 @@
 import os
+import sys
+import mysql.connector
+import getpass
+import readchar
+from mysql.connector import Error
 
+#Connect to db, and set cursor
+def hstack_connect(u_name, p_word):
+    try:
+        db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = u_name, password = p_word) #attempt db connection
+    except Error as e:
+        print(e) #if connection fails, print error
+    if db_con.is_connected():
+        return db_con #if connected, return connection
+    else:
+        sys.exit("Couldn't Connect") #exit
+
+#Clear screen function
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+
+def dot_echo():
+    pass_list = []
+    key_stroke = readchar.readkey()
+    while key_stroke != readchar.key.ENTER:
+        pass_list.append(key_stroke)
+        sys.stdout.write('.')
+        key_stroke = readchar.readkey()
+    secret = ''.join(pass_list)
+    return secret
+
+stuff = dot_echo()
+print(stuff)
