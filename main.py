2336701fbfa8cfcac28a33b5856a1207fb6cbb93
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-14 14:55:08 -0400

    changed dot_echo to echo_char. Now allows a string to be passed. Defaults to .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ password = main_func.getpass.getpass("Enter Password: ")
 con = main_func.hstack_connect(username, password)
 #main menu
 #main_func.cls()
-#print ('Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients')
+#print ("Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients\n5. Log Out")
 #menu_select = input("Select a Menu: ")
 
 if con is not None and con.is_connected():
--- main_func.py
+++ main_func.py
@@ -5,7 +5,7 @@ import getpass
 import readchar
 from mysql.connector import Error
 
-#Connect to db, and set cursor
+#Connect to db
 def hstack_connect(u_name, p_word):
     try:
         db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = u_name, password = p_word) #attempt db connection
@@ -20,15 +20,15 @@ def hstack_connect(u_name, p_word):
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def dot_echo():
+#Function takes in user input, and returns a string, while echoing a single char to the console
+#Char to echo defaults to . but can be passed as an argument
+def echo_char(echo_char = '.'):
     pass_list = []
     key_stroke = readchar.readkey()
     while key_stroke != readchar.key.ENTER:
         pass_list.append(key_stroke)
-        print('.', end = '', flush = True)
+        print(echo_char, end = '', flush = True)#echoes a single character to the console without newline
         key_stroke = readchar.readkey()
-    secret = ''.join(pass_list)
+    secret = ''.join(pass_list)#convert pass_list to a string to return
     return secret
 
-stuff = dot_echo()
-print(stuff)
