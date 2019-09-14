6bb206dc5f9f4f119858cee2d8d94bae8dca1052
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-14 17:22:56 -0400

    changed echo_char to respect backspacing. Added cursor_index to avoid backspacing prompt

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import main_func
 
-username = input("Enter Username: ")
-password = main_func.getpass.getpass("Enter Password: ")
+#log in
+connect = main_func.login()
 
-#establish db connection
-con = main_func.hstack_connect(username, password)
 #main menu
-#main_func.cls()
-#print ("Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients\n5. Log Out")
-#menu_select = input("Select a Menu: ")
+main_func.cls()
+print ("Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients\n5. Log Out")
+menu_select = input("Select a Menu: ")
 
-if con is not None and con.is_connected():
-    con.close()
+if connect is not None and connect.is_connected():
+    connect.close()
--- main_func.py
+++ main_func.py
@@ -11,10 +11,9 @@ def hstack_connect(u_name, p_word):
         db_con = mysql.connector.connect(host = 'localhost', database = 'hopstack', user = u_name, password = p_word) #attempt db connection
     except Error as e:
         print(e) #if connection fails, print error
-    if db_con.is_connected():
-        return db_con #if connected, return connection
-    else:
         sys.exit("Couldn't Connect") #exit
+    
+    return db_con #if connected, return connection
 
 #Clear screen function
 def cls():
@@ -22,13 +21,28 @@ def cls():
 
 #Function takes in user input, and returns a string, while echoing a single char to the console
 #Char to echo defaults to . but can be passed as an argument
-def echo_char(echo_char = '.'):
+def echo_char(prompt = None, echo_char = '.'):
+    cursor_index = 0
     pass_list = []
+    if prompt != None:
+        print(prompt, end = '', flush = True)#if there's a prompt, print it
     key_stroke = readchar.readkey()
-    while key_stroke != readchar.key.ENTER:
-        pass_list.append(key_stroke)
-        print(echo_char, end = '', flush = True)#echoes a single character to the console without newline
+    while key_stroke != readchar.key.ENTER:#keep geting input until ENTER
+        if key_stroke == '\x08':
+            if cursor_index > 0:
+                del pass_list[-1]
+                print('\b \b', end = '', flush = True)#move cursor back, overwrite with blank in event of
+                cursor_index -= 1
+        else:
+            pass_list.append(key_stroke)
+            print(echo_char, end = '', flush = True)#echoes string to the console without newline
+            cursor_index += 1
         key_stroke = readchar.readkey()
     secret = ''.join(pass_list)#convert pass_list to a string to return
     return secret
 
+def login():
+    username = input("Enter Username: ")
+    password = echo_char("Enter Password: ")
+    con = hstack_connect(username, password)
+    return con
