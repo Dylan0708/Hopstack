a9f66c9380d3dd882c95965fc065245f4876a3ab
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-14 20:41:29 -0400

    Added logout and exit to main menu

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import main_func
 
-#log in
-main_func.cls()
-connect = main_func.login()
+menu_select = '5' 
 
 #main menu
-main_func.cls()
-print ("Hopstack Main Menu\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients\n5. Log Out")
-menu_select = input("Select a Menu: ")
+def main_menu():
+    main_func.cls()
+    print ("Hopstack Main Menu\n\n1. Inventory\n2. Recipes\n3. Shopping Lists\n4. Ingredients\n5. Log Out\n6. Exit")
+    select = input("Select a Menu: ")
+    return select
+
+#log in
+while menu_select != '6':
+    if menu_select == '5':
+        main_func.cls()
+        connect = main_func.login()
+        menu_select = main_menu()
 
 if connect is not None and connect.is_connected():
     connect.close()
+ 
+
