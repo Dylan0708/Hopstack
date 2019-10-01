6b137209ac138e60bec60285dd6e00b034a28371
Dylan Dykeman <ddykeman1991@gmail.com>
2019-09-30 21:45:17 -0400

    reformatted some comments

--- modules/misc_methods.py
+++ modules/misc_methods.py
@@ -6,19 +6,27 @@ def cls():
 def echo_char(prompt = None, echo_char = '.'):
     cursor_index = 0
     pass_list = []
+
     if prompt != None:
-        print(prompt, end = '', flush = True)  # if there's a prompt, print it
+        # if there's a prompt, print it
+        print(prompt, end = '', flush = True)
+
     key_stroke = readchar.readkey()
-    while key_stroke != readchar.key.ENTER:  # keep geting input until ENTER
+    # keep geting input until ENTER
+    while key_stroke != readchar.key.ENTER:
         if key_stroke == '\x08':
             if cursor_index > 0:
                 del pass_list[-1]
-                print('\b \b', end = '', flush = True)  # move cursor back, overwrite with blank in event of
+                # move cursor back, overwrite with blank in event of
+                print('\b \b', end = '', flush = True)
                 cursor_index -= 1
         else:
             pass_list.append(key_stroke)
-            print(echo_char, end = '', flush = True)  # echoes string to the console without newline
+            # echoes string to the console without newline
+            print(echo_char, end = '', flush = True)
             cursor_index += 1
         key_stroke = readchar.readkey()
-    secret = ''.join(pass_list)  # convert pass_list to a string to return
+        
+    # convert pass_list to a string to return
+    secret = ''.join(pass_list)
     return secret
